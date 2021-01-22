66ac962969367eba4357fabf55059c269f8f2f9c
mxdl <geduo_83@163.com>
2021-01-22 18:28:40 +0800

    coroutine test

--- app/src/main/java/com/mxdl/kotlin/coroutine1/14利用协程干掉try-catch.kt
+++ app/src/main/java/com/mxdl/kotlin/coroutine1/14利用协程干掉try-catch.kt
@@ -8,25 +8,77 @@ import kotlinx.coroutines.*
  * Update:     <br>
  */
 fun main(args: Array<String>) {
+    //testException()
+    //main()
+    //test1()
+    test2()
+    Thread.sleep(1000 * 5)
+}
+
+private fun test2() {
+    runBlocking {
+
+        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println(throwable)
+        }
+        val coroutineScope = CoroutineScope(Job() + handler)
+        coroutineScope.launch {
+            launch {
+                println("launcher1 start")
+                delay(500)
+                throw Exception("launcher1 e")
+            }
+            launch {
+                delay(1000)
+                println("launcher2 end")
+                throw Exception("launcher2 e")
+            }
+            delay(1000)
+            println("launcher end")
+        }
+        println("runBlocking end")
+    }
+}
+
+private fun test1() {
+    val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        println(throwable)
+    }
+    runBlocking {
+        supervisorScope {
+            launch(handler) {
+                println("launcher1 start")
+                delay(500)
+                throw Exception("launcher1 e")
+            }
+            launch(handler) {
+                println("launcher2 start")
+                delay(1000)
+                throw Exception("launcher3 e")
+            }
+            println("launcher3 end")
+        }
+        println("runBlocking end")
+    }
+}
+
+private fun testException() {
     //1.永远不要跨域捕获异常
     //2.利用CoroutineExceptionHandler可以简化异常捕获
     //3.CoroutineExceptionHanlder只能放在顶级协程
-    val coroutineScope = CoroutineScope(Job() + CoroutineExceptionHandler { coroutineContext, throwable ->
-        println(throwable)
-    })
+    val coroutineScope =
+        CoroutineScope(Job() + CoroutineExceptionHandler { coroutineContext, throwable ->
+            println(throwable)
+        })
     coroutineScope.launch {
-        try {
-            launch {
-                delay(500)
-                throw Exception("Exception1")
-            }
-            launch {
-                delay(200)
-                throw Exception("Exception2")
-            }
-        } catch (e: Exception) {
-            println(1)
+        launch {
+            delay(500)
+            throw Exception("Exception1")
+        }
+        launch {
+            delay(200)
+            throw Exception("Exception2")
         }
     }
-    Thread.sleep(1000 * 5)
 }
+
